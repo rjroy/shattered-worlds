f384e6c16bcc138a42da419e86b894cc905a5aec
Ronald Roy <gsdwig@gmail.com>
2026-06-07 07:54:54 -0700

    refactor: simplify test fixture by directly using buildWorld for worldData and catalog

--- src/core/tests/testFixture.ts
+++ src/core/tests/testFixture.ts
@@ -2,12 +2,6 @@
  * Shared test fixture: assembles catalog and worldData once for use across
  * all core test files. Import from here instead of duplicating setup.
  */
-import { worldManifest } from '../../data/worldManifest'
+import { buildWorld } from '../../data/worldManifest'
 
-export const { catalog, worldData } = function () {
-    const worldBuilder = worldManifest['zombie-big-box']
-    if (typeof worldBuilder !== 'function') {
-      throw new Error('World builder for "zombie-big-box" is not a function')
-    }
-    return worldBuilder()
-}()
+export const { catalog, worldData } = buildWorld('zombie-big-box')
