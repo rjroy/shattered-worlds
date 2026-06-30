990a0fb1506088daa1642c6ee31898ffe755f5a9
Ronald Roy <gsdwig@gmail.com>
2026-06-29 22:23:33 -0700

    added unlock icon

--- src/game/data/assetManifest.ts
+++ src/game/data/assetManifest.ts
@@ -70,12 +70,12 @@ import unlockWorldWhiteoutParkingGarageUrl from "../assets/unlocks/world-whiteou
 import unlockWorldTheTidalArchiveUrl from "../assets/unlocks/world-the-tidal-archive.webp";
 import unlockWorldTheEmberOrchardUrl from "../assets/unlocks/world-the-ember-orchard.webp";
 import unlockWorldCityOfSleepingGiantsUrl from "../assets/unlocks/world-city-of-sleeping-giants.webp";
+import unlockWorldEdenPrimeUrl from "../assets/unlocks/world-eden-prime.webp";
 import vignetteUrl from "../assets/vignette.png";
 import worldSelectBgUrl from "../assets/world-select.webp";
 import screenChronicleUrl from "../assets/screens/shattered-chronicle.webp";
 import screenDestinyUrl from "../assets/screens/shattered-destiny.webp";
 import screenLoseUrl from "../assets/screens/shattered-lose.webp";
-import edenPrimeCardfrontUrl from "../assets/themes/eden-prime/eden-prime-cardfront.webp";
 import theDoorFxUrl from "../assets/audio/fx/mystical-lightning.mp3";
 import zombieFxUrl from "../assets/audio/fx/zombie-moan.mp3";
 import floorItFxUrl from "../assets/audio/fx/floor-it.mp3";
@@ -161,7 +161,7 @@ export const assetManifest: Record<string, string> = {
   "unlock/world-the-tidal-archive": unlockWorldTheTidalArchiveUrl,
   "unlock/world-the-ember-orchard": unlockWorldTheEmberOrchardUrl,
   "unlock/world-city-of-sleeping-giants": unlockWorldCityOfSleepingGiantsUrl,
-  "unlock/world-eden-prime": edenPrimeCardfrontUrl,
+  "unlock/world-eden-prime": unlockWorldEdenPrimeUrl,
   "world-select-bg": worldSelectBgUrl,
   vignette: vignetteUrl,
   "screen-chronicle": screenChronicleUrl,
